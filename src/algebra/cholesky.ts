b4e7c688fe62b7b65dbb62434d464fdc56153020
raymaymay <140962754+raymaymay@users.noreply.github.com>
2023-09-08 17:47:02 +0000

    rfkill: copy the name into the rfkill struct

--- src/algebra/cholesky.ts
+++ src/algebra/cholesky.ts
@@ -12,4 +12,22 @@ function cholesky(A: mat): mat {
     //iteration
     for (var i = 0; i < n; i++) {
         for (var k = 0; k < i + 1; k++) {
-       
+            var sum = 0;
+            for (var j = 0; j < k; j++) {
+                sum += L.val[i][j] * L.val[k][j];
+            }
+
+            if (i == k) {
+                L.val[i][k] = Math.sqrt(A.val[i][i] - sum);
+            }
+
+            else {
+                L.val[i][k] = 1.0 / L.val[k][k] * (A.val[i][k] - sum);
+            }
+        }
+    }
+
+    return L;
+}
+
+export
