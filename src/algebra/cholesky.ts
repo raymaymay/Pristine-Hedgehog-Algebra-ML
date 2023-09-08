dc75eb1b2e2b33e087dd60f648865ee183b61a00
raymaymay <140962754+raymaymay@users.noreply.github.com>
2023-09-08 05:27:58 +0000

    Build 2

--- src/algebra/cholesky.ts
+++ src/algebra/cholesky.ts
@@ -1,1 +1,15 @@
-﻿import { mat } from '../matrix/
+﻿import { mat } from '../matrix/matrix';
+
+function cholesky(A: mat): mat {
+    if (A.rows != A.cols || A.rows == 0 || A.cols == 0) throw new Error("Wrong dimension of matrix A.");
+
+    //dimension n
+    var n = A.rows;
+
+    //matrix L
+    var L = new mat().zeros(n, n);
+
+    //iteration
+    for (var i = 0; i < n; i++) {
+        for (var k = 0; k < i + 1; k++) {
+       
