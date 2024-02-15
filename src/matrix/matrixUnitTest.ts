df2d8d0397e72db68e5ac1903c43e6cda1bb0f82
raymaymay <140962754+raymaymay@users.noreply.github.com>
2024-02-15 17:47:19 +0000

    new images added

--- src/matrix/matrixUnitTest.ts
+++ src/matrix/matrixUnitTest.ts
@@ -1,1 +1,15 @@
-﻿import{ add, addInPlace, minus, minusInPlace, mul, mulInPlace, mul_gpu, x_mul_xT, xT_mul_x, dotMul, dot
+﻿import{ add, addInPlace, minus, minusInPlace, mul, mulInPlace, mul_gpu, x_mul_xT, xT_mul_x, dotMul, dotMulInPlace, mat} from './matrix';
+import {mean, std } from './vector';
+var assert = require('assert');
+
+export function matrixTest() {
+    matrixAllocationTest();
+    matrixClearTest();
+    matrixVectorInitializationTest();
+    matrixRangeTest();
+    matrixCloneAndCopyTest();
+    matrixInitTest();
+    matrixOperationTest();
+}
+
+function vcecto
