28867f971c627bca9344f734b700ad4f06b18cfe
raymaymay <140962754+raymaymay@users.noreply.github.com>
2024-03-22 18:05:02 +0000

    In development

--- src/ml/linearRegression.ts
+++ src/ml/linearRegression.ts
@@ -3,4 +3,15 @@ import { inverse } from '../algebra/inverse';
 
 
 //reference: https://zhuanlan.zhihu.com/p/25434586
-//api design: https://scikit-learn.org/stable/modules/gen
+//api design: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
+//https://www.mathworks.com/matlabcentral/fileexchange/64930-linear-regression-simplest-implementation
+
+class LinearRegression {
+
+    w: mat;
+
+    constructor() { }
+
+    //x: M-by-N matrix. M data with N dimensions. Each row is an N-dim vector
+    //y: M-by-1 matrix
+    fit(x_: mat, y_: mat) 
