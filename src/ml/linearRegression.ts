68e1fe389b610cd23c275819e64404768fb61453
raymaymay <140962754+raymaymay@users.noreply.github.com>
2024-04-10 09:44:34 +0000

    Refactor controllers

--- src/ml/linearRegression.ts
+++ src/ml/linearRegression.ts
@@ -14,4 +14,19 @@ class LinearRegression {
 
     //x: M-by-N matrix. M data with N dimensions. Each row is an N-dim vector
     //y: M-by-1 matrix
-    fit(x_: mat, y_: mat) 
+    fit(x_: mat, y_: mat) : LinearRegression{
+
+        var y = y_;
+        //check the dimension of y
+        if (y_.rows != 1 && y_.cols == 1) {
+            y = y_.T();
+        }
+
+
+        //expan x_ with one more column with 1
+        var x = x_.resize(x_.rows, x_.cols + 1, 1);
+
+        //calculate w = (X.T() * X)^-1 * X.T() * y
+
+        //(X.T() * X)
+        var xT_mul_x =
